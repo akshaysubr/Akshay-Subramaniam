f6b634bdc76074e5942be19bbb621a5bd1c3dea0
Akshay Subramaniam <akshaysubr@gmail.com>
2015-01-20 09:03:16 -0800

    Changed structure to include a prob folder with the problem specific postprocessing scripts

--- build/prob/cmake_install.cmake
+++ build/prob/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/modern4/akshays/mir-pp/src
+# Install script for directory: /home/modern4/akshays/mir-pp/src/prob
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,19 +32,3 @@ IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   SET(CMAKE_INSTALL_SO_NO_EXE "0")
 ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
 
-IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  INCLUDE("/home/modern4/akshays/mir-pp/build/prob/cmake_install.cmake")
-
-ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
-
-IF(CMAKE_INSTALL_COMPONENT)
-  SET(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-ELSE(CMAKE_INSTALL_COMPONENT)
-  SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-ENDIF(CMAKE_INSTALL_COMPONENT)
-
-FILE(WRITE "/home/modern4/akshays/mir-pp/build/${CMAKE_INSTALL_MANIFEST}" "")
-FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/modern4/akshays/mir-pp/build/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
-ENDFOREACH(file)
